6065a93061ebb99a5e8aae35bccde3d409eb33ca
Williford Abunas <142576037+williford-abunas@users.noreply.github.com>
2023-11-17 11:38:59 +1300

    db functions

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,6 +1,29 @@
 import connection from './connection.ts'
-import { Fruit } from '../../models/fruit.ts'
+import { Fruit, Dance } from '../../models/Dances.ts'
 
 export async function getAllFruits(db = connection): Promise<Fruit[]> {
   return db('fruit').select()
+}
+
+export async function getAllDances(db = connection): Promise<Dance[]> {
+  return db('Dances').select('*', 'is_complete as isComplete')
+}
+
+export async function getDancebyId(
+  { id }: Dance,
+  db = connection,
+): Promise<Dance[]> {
+  return db('Dances').select('*', 'is_complete as isComplete').where('id', id)
+    .first
+}
+
+export async function countCompletion(db = connection): Promise<Dance[]> {
+  return db('Dances').select('is_complete as isComplete')
+}
+
+export async function updateCompletion(
+  { id, isComplete }: Dance,
+  db = connection,
+): Promise<Dance[]> {
+  return db('Dances').update({ isComplete: isComplete }).where('id', id)
 }
