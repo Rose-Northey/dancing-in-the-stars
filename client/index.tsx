9fe7fe5d47f44d10b0b45143d57c7c891c24f65d
Rose-Northey <132635605+Rose-Northey@users.noreply.github.com>
2023-11-17 10:30:21 +1300

    add router provider

--- client/index.tsx
+++ client/index.tsx
@@ -1,16 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { routes } from './routes.tsx'
 
-import App from './components/App.tsx'
 
 const queryClient = new QueryClient()
+const router = createBrowserRouter(routes)
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools />
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   )
 })
