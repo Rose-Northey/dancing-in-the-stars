694350255c58d37ece8f459b014833167f1902d0
Rose-Northey <132635605+Rose-Northey@users.noreply.github.com>
2023-11-17 11:55:55 +1300

    add getDanceDetails function

--- client/apis/apiClient.ts
+++ client/apis/apiClient.ts
@@ -15,9 +15,28 @@ export async function getAllDances() {
       isComplete: false,
     },
   ]
-  console.log(data)
+
   return data
   // return request.get(rootUrl + '/dances').then((res) => {
+  //   return res.body
+  // })
+}
+
+export async function getDanceDetails(id) {
+  const data = {
+    id: 1,
+    name: 'Orange Justice',
+    description: 'blah blah blah',
+    link: 'youtube url',
+    isComplete: false,
+  }
+  console.log()
+
+  return data
+
+  //make a request for route 	/v1/dances/:id
+
+  // return request.get(rootUrl + `dances/${id}`).then((res) => {
   //   return res.body
   // })
 }
