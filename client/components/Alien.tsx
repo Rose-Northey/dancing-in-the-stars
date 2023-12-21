7466d6d1989225afa8e4c7b62b4bd6e0726b28c3
rose northey <roseAnorthey@gmail.com>
2023-12-22 12:14:03 +1300

    more responsive styling

--- client/components/Alien.tsx
+++ client/components/Alien.tsx
@@ -1,22 +1,15 @@
 export default function Alien() {
   return (<>
-    
     <div className="alien-container">
       <h2>Skinsuit Progress</h2>
-      <div className="alien head">
-        <img src="public/aHead.PNG" alt="Alien head"></img>
-      </div>
-
-      <div className="alien body">
-        <img src="public/aBody.PNG" alt="alien body"></img>
-      </div>
-
-      <div className="alien legs">
-        <img src="public/aKnees.PNG" alt="alien legs"></img>
-      </div>
-
-      <div className="alien feet">
-        <img src="public/aFeet.PNG" alt="alien feet"></img>
+      <div className='alien-container-lower'>
+        <div className="spacer"></div>
+        <div className='alien'>
+          <img className="head" src="public/aHead.PNG" alt="Alien head"></img>
+          <img className="body" src="public/aBody.PNG" alt="alien body"></img>
+          <img className="legs" src="public/aKnees.PNG" alt="alien legs"></img>
+          <img className="feet"src="public/aFeet.PNG" alt="alien feet"></img>
+        </div>
       </div>
     </div>
     </>
