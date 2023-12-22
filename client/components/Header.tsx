68b969381baa564da44d0b8c8b2c09703d3c93e0
rose northey <roseAnorthey@gmail.com>
2023-12-22 17:40:50 +1300

    home button works

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,12 +1,11 @@
+import {Link} from 'react-router-dom'
 export default function Header(){
-
-
 
   return( <>
       <header>
         <div>
           <h1>Dancing In The Stars</h1>
-          <button><p>Home</p></button>
+          <Link to='/'><button><p>Home</p></button></Link>
         </div>
         <p className= "subheader">become human through dance</p>
       </header>
